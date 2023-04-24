8b43b519bac366f8ce60218ac915d5d615dd90f0
DirtyKeyboard <andrewxx000222@icloud.com>
2023-04-24 17:21:47 -0500

    d

--- server/models.py
+++ server/models.py
@@ -12,9 +12,24 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validate_name(self, key, val):
+        if len(val) == 0:
+            raise ValueError("NO!!")
+        return val
+
+    @validates('phone_number')
+    def validate_number(self, key, val):
+        if len(val) != 10:
+            raise ValueError("d")
+        return val
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
-
+"""
+Post content is at least 250 characters long.
+Post summary is a maximum of 250 characters.
+"""
 class Post(db.Model):
     __tablename__ = 'posts'
     # Add validations and constraints 
@@ -27,6 +42,32 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('title')
+    def validate_title(self, key, val):
+        if len(val) == 0 or "top" not in val.lower() or "guess" not in val.lower() or "secret" not in val.lower() or "won't believe" not in val.lower():
+            raise ValueError("d")
+        return val
+    
+    @validates('category')
+    def validate_category(self, key, val):
+        if val.lower() == 'fiction' or val.lower() == 'non-fiction':
+            return val
+        else:
+            raise ValueError("nope")
+        
+    @validates('content')
+    def validate_content(self, key, val):
+        if len(val) < 250:
+            raise ValueError("no")
+        return val
+            
+
+    @validates('summary')
+    def validate_summary(self, key, val):
+        if len(val) >= 250:
+            raise ValueError("no")
+        return val
+            
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
